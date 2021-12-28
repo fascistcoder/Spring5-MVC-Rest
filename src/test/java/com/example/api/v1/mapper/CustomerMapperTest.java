37a058944ee86ad174052981b7cdaebb04a5c061
Pulkit Aggarwal <gargpulkit234@gmail.com>
2021-12-28 16:10:50 +0530

    Customer Test

---
+++ src/test/java/com/example/api/v1/mapper/CustomerMapperTest.java
@@ -0,0 +1,37 @@
+package com.example.api.v1.mapper;
+
+import com.example.api.v1.model.CustomerDTO;
+import com.example.domain.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 28/12/21
+ */
+class CustomerMapperTest {
+
+    public static final String FIRSTNAME = "Joe";
+    public static final String LASTNAME = "Root";
+
+    CustomerMapper customerMapper = CustomerMapper.INSTANCE;
+
+    @Test
+    void customerToCustomerDTO() {
+
+        //given
+        Customer customer = new Customer();
+        customer.setFirstname(FIRSTNAME);
+        customer.setLastname(LASTNAME);
+
+        //when
+        CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+
+        //then
+        assertEquals(FIRSTNAME, customerDTO.getFirstname());
+        assertEquals(LASTNAME, customerDTO.getLastname());
+    }
+}
---
+++ src/test/java/com/example/services/CustomerServiceImplTest.java
@@ -0,0 +1,79 @@
+package com.example.services;
+
+import com.example.api.v1.mapper.CustomerMapper;
+import com.example.api.v1.model.CustomerDTO;
+import com.example.domain.Customer;
+import com.example.repositories.CustomerRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 28/12/21
+ */
+class CustomerServiceImplTest {
+
+    @Mock
+    CustomerRepository customerRepository;
+
+    CustomerMapper customerMapper = CustomerMapper.INSTANCE;
+
+    CustomerService customerService;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+
+        customerService = new CustomerServiceImpl(customerMapper, customerRepository);
+    }
+
+    @Test
+    void getAllCustomers() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1L);
+        customer1.setFirstname("Michale");
+        customer1.setLastname("Weston");
+
+        Customer customer2 = new Customer();
+        customer2.setId(2L);
+        customer2.setFirstname("Sam");
+        customer2.setLastname("Axe");
+
+        when(customerRepository.findAll()).thenReturn(Arrays.asList(customer1, customer2));
+
+        //when
+        List<CustomerDTO> customerDTOS = customerService.getAllCustomers();
+
+        //then
+        assertEquals(2, customerDTOS.size());
+
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1L);
+        customer1.setFirstname("Michale");
+        customer1.setLastname("Weston");
+
+        when(customerRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(customer1));
+
+        //when
+        CustomerDTO customerDTO = customerService.getCustomerById(1L);
+
+        assertEquals("Michale", customerDTO.getFirstname());
+    }
+}

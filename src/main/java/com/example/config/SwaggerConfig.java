00b560c95588052871b936ecd7a4b45d8f428be9
Pulkit Aggarwal <gargpulkit234@gmail.com>
2022-03-03 12:31:43 +0530

    Swagger Implementation

---
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 03/03/22
+ */
+@Configuration
+public class SwaggerConfig {
+}
